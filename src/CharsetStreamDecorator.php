64828a1b53372df1448b3692f42d2d206af4ef94
Zaahid Bateson <zbateson@gmail.com>
2018-02-17 23:26:42 -0800

    Add CharsetStreamDecorator

---
+++ src/CharsetStreamDecorator.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * This file is part of the ZBateson\StreamDecorator project.
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace ZBateson\StreamDecorators;
+
+use Psr\Http\Message\StreamInterface;
+use ZBateson\StreamDecorators\Util\CharsetConverter;
+
+/**
+ * GuzzleHttp\Psr7 stream decoder extension for base64 streams.
+ *
+ * Extends AbstractMimeTransferStreamDecorator, which prevents getSize and
+ * seeking to anywhere except the beginning (rewinding).
+ *
+ * The size of the underlying stream and the position of chars can't be
+ * determined because some charsets (e.g. UTF-8), may take between 1 and 6(?) or
+ * so bytes, and without reading them they are not known.
+ *
+ * @author Zaahid Bateson
+ */
+class CharsetStreamDecorator extends AbstractMimeTransferStreamDecorator
+{
+    /**
+     * @var \ZBateson\StreamDecorators\Util\CharsetConverter the charset
+     *      converter
+     */
+    protected $converter = null;
+    
+    /**
+     * @var string charset of the source stream
+     */
+    protected $fromCharset = 'ISO-8859-1';
+    
+    /**
+     * @var string charset to convert to
+     */
+    protected $toCharset = 'UTF-8';
+
+    /**
+     * @var int number of characters in $buffer
+     */
+    private $bufferLength = 0;
+
+    /**
+     * @var string a buffer of characters read in the original $fromCharset
+     *      encoding
+     */
+    private $buffer = '';
+
+    /**
+     * @param StreamInterface $stream Stream to decorate
+     * @param string $fromCharset The underlying stream's charset
+     * @param string $toCharset The charset to encode to
+     */
+    public function __construct(StreamInterface $stream, $fromCharset = 'ISO-8859-1', $toCharset = 'UTF-8')
+    {
+        parent::__construct($stream);
+        $this->converter = new CharsetConverter();
+        $this->fromCharset = $fromCharset;
+        $this->toCharset = $toCharset;
+    }
+
+    /**
+     * Reads a minimum of $length characters from the underlying stream in its
+     * encoding into $this->buffer
+     *
+     * @param int $length
+     */
+    private function readRawCharsIntoBuffer($length)
+    {
+        $n = $length + 32;
+        while ($this->bufferLength < $n) {
+            $raw = $this->readRaw($n + 512);
+            if ($raw === false || $raw === '') {
+                return;
+            }
+            $this->buffer .= $raw;
+            $this->bufferLength = $this->converter->getLength($this->buffer, $this->fromCharset);
+        }
+    }
+
+    /**
+     * Returns true if the end of stream has been reached.
+     *
+     * @return type
+     */
+    public function eof()
+    {
+        return ($this->bufferLength === 0 && parent::eof());
+    }
+
+    /**
+     * Reads up to $length decoded bytes from the underlying quoted-printable
+     * encoded stream and returns them.
+     *
+     * @param int $length
+     * @return string
+     */
+    public function read($length)
+    {
+        // let Guzzle decide what to do.
+        if ($length <= 0 || $this->eof()) {
+            return $this->readRaw($length);
+        }
+        $this->readRawCharsIntoBuffer($length);
+        $numChars = min([$this->bufferLength, $length]);
+        $chars = $this->converter->getSubstr($this->buffer, $this->fromCharset, 0, $numChars);
+        
+        $this->position += $numChars;
+        $this->buffer = $this->converter->getSubstr($this->buffer, $this->fromCharset, $numChars);
+        $this->bufferLength = $this->bufferLength - $numChars;
+
+        return $this->converter->convert($chars, $this->fromCharset, $this->toCharset);
+    }
+
+    public function write($string)
+    {
+        // not implemented yet
+    }
+}
---
+++ tests/StreamDecorators/CharsetStreamDecoratorTest.php
@@ -0,0 +1,105 @@
+<?php
+namespace ZBateson\StreamDecorators;
+
+use PHPUnit_Framework_TestCase;
+use GuzzleHttp\Psr7;
+use ZBateson\StreamDecorators\Util\CharsetConverter;
+
+/**
+ * Description of CharsetStreamDecoratorTest
+ *
+ * @group CharsetStreamDecorator
+ * @covers ZBateson\StreamDecorators\CharsetStreamDecorator
+ * @author Zaahid Bateson
+ */
+class CharsetStreamDecoratorTest extends PHPUnit_Framework_TestCase
+{
+    private $converter;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->converter = new CharsetConverter();
+    }
+
+    public function testReadAndRewind()
+    {
+        $str = str_repeat('هلا هلا شخبار بعد؟ شلون تبرمج؟', 100);
+
+        $stream = Psr7\stream_for($this->converter->convert($str, 'UTF-8', 'UTF-32'));
+        $qpStream = new CharsetStreamDecorator($stream, 'UTF-32', 'UTF-8');
+
+        for ($i = 1; $i < mb_strlen($str); ++$i) {
+            $qpStream->rewind();
+            for ($j = 0; $j < mb_strlen($str); $j += $i) {
+                $char = $qpStream->read($i);
+                $this->assertEquals(mb_substr($str, $j, $i), $char, "Read $j failed at $i step");
+            }
+            $this->assertEquals(mb_strlen($str), $qpStream->tell(), "Final tell failed with $i step");
+        }
+    }
+
+    public function testReadContents()
+    {
+        $str = str_repeat('هلا هلا شخبار بعد؟ شلون تبرمج؟', 100);
+
+        for ($i = 0; $i < mb_strlen($str); ++$i) {
+            $substr = mb_substr($str, 0, $i + 1, 'UTF-8');
+            $stream = Psr7\stream_for($this->converter->convert($substr, 'UTF-8', 'UTF-16'));
+            $qpStream = new CharsetStreamDecorator($stream, 'UTF-16', 'UTF-8');
+            $this->assertEquals($substr, $qpStream->getContents());
+        }
+    }
+
+    public function testReadToEof()
+    {
+        $str = str_repeat('هلا هلا شخبار بعد؟ شلون تبرمج؟', 10);
+        for ($i = 0; $i < mb_strlen($str, 'UTF-8'); ++$i) {
+            $substr = mb_substr($str, $i, null, 'UTF-8');
+            $stream = Psr7\stream_for($this->converter->convert($substr, 'UTF-8', 'WINDOWS-1256'));
+            $qpStream = new CharsetStreamDecorator($stream, 'WINDOWS-1256', 'UTF-8');
+            for ($j = 0; !$qpStream->eof(); ++$j) {
+                $read = $qpStream->read(1);
+                $this->assertEquals(mb_substr($substr, $j, 1, 'UTF-8'), $read, "Failed reading to EOF on substr $i iteration $j");
+            }
+        }
+    }
+
+    public function testGetSize()
+    {
+        $str = 'Wubalubadubduuuuuuuuuuuuuuuuuuuuuuuuuuuuub!';
+
+        $stream = Psr7\stream_for($this->converter->convert($str, 'UTF-8', 'WINDOWS-1256'));
+        $qpStream = new CharsetStreamDecorator($stream, 'WINDOWS-1256', 'UTF-8');
+        $this->assertNull($qpStream->getSize());
+    }
+
+    public function testTell()
+    {
+        $str = 'é J\'interdis aux marchands de vanter trop leur marchandises. Car '
+            . 'ils se font vite pédagogues et t\'enseignent comme but ce qui '
+            . 'n\'est par essence qu\'un moyen, et te trompant ainsi sur la '
+            . 'route à suivre les voilà bientôt qui te dégradent, car si leur '
+            . 'musique est vulgaire ils te fabriquent pour te la vendre une âme '
+            . 'vulgaire.é';
+        $stream = Psr7\stream_for($str);
+        $qpStream = new CharsetStreamDecorator($stream);
+
+        for ($i = 1; $i < strlen($str); ++$i) {
+            $qpStream->rewind();
+            for ($j = 0; $j < strlen($str); $j += $i) {
+                $this->assertEquals($j, $qpStream->tell(), "Tell at $j failed with $i step");
+                $qpStream->read($i);
+            }
+            $this->assertEquals(strlen($str), $qpStream->tell(), "Final tell failed with $i step");
+        }
+    }
+
+    public function testSeek()
+    {
+        $this->setExpectedException('RuntimeException');
+        $stream = Psr7\stream_for('test');
+        $qpStream = new Base64StreamDecorator($stream);
+        $qpStream->seek(10);
+    }
+}
